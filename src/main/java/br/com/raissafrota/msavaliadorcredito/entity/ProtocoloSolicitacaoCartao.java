bcf0e19847613eafcc7f8d485ed5fff77203d602
Raissa <raissa.frota@capgemini.com>
2023-03-20 13:35:36 -0300

    Criação do publisher e da fila da mensageria

---
+++ src/main/java/br/com/raissafrota/msavaliadorcredito/entity/ProtocoloSolicitacaoCartao.java
@@ -0,0 +1,10 @@
+package br.com.raissafrota.msavaliadorcredito.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProtocoloSolicitacaoCartao {
+    private String protocolo;
+}
